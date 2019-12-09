b2f842e2134c7283b55759a44d20f02044fb3b3e
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-12-09 08:26:55 -0800

    Added DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i| 
+    Blog.create!(
+        title: "My Blog Post #{i}",
+        body: "Lorem ipsum dolor sit amet, ...."
+    )
+end
+
+puts "Blog posts created"
+
+5.times do |i| 
+    Skill.create!(
+        title: "Rails #{i}",
+        utilized: 15
+    )
+end
+
+puts "Skills created"
+
+5.times do |i| 
+    Portfolio.create!(
+        title: "Porfolio Title #{i}",
+        subtitle: "Porfolio Subtitle #{i}", 
+        body: "Lorem Ipsum Body Text #{i}",
+        main_image: "https://via.placeholder.com/600x400",
+        thumb_image: "https://via.placeholder.com/350x200"
+    )
+end
+
+puts "Portfolio items created"

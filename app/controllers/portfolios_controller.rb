7dc9a7c5aba5a9c927b098d2cb6e0bdb03ddd48d
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-12-11 08:31:31 -0800

    Added new and create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,19 @@ class PortfoliosController < ApplicationController
         @portfolio_items = Portfolio.all
     end
 
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body)) 
+
+        respond_to do |format| 
+            if @portfolio_item.save
+                format.html { redirect_to portfolios_path, notice: 'Your portfolio_item is now live'}
+            else  
+                format.html { render :new }
+            end
+        end
+    end
 end

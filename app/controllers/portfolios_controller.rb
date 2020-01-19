8a58d6e9d79f9e201d483939a496f5589f5d33e1
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-01-19 07:30:34 -0800

    Working with strong params in a rails 5 controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,11 +10,11 @@ class PortfoliosController < ApplicationController
 
     def new
         @portfolio_item = Portfolio.new
-        3.times { @portfolio_item.technologies.build} #create three types of technologies and make those available to the form
+        3.times { @portfolio_item.technologies.build} 
     end
 
     def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])) #
+        @portfolio_item = Portfolio.new(portfolio_params) #
 
         respond_to do |format| 
             if @portfolio_item.save
@@ -32,7 +32,7 @@ class PortfoliosController < ApplicationController
     def update
         @portfolio_item = Portfolio.find(params[:id])
         respond_to do |format| 
-            if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+            if @portfolio_item.update(portfolio_params)
                 format.html { redirect_to portfolios_path, notice: 'Your portfolio_item is now live'}
             else  
                 format.html { render :edit }
@@ -56,7 +56,11 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio Item was successfully destroyed.' }
     end
-
   end
 
+  private 
+
+  def portfolio_params 
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ Bundler.require(*Rails.groups)
 
 module DevCampPortfolio
   class Application < Rails::Application
+    # this is how to override the defaults. generally not needed.
+    # config.action_controller.permit_all_parameters = true 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 

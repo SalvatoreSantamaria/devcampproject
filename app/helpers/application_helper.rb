e7b524319135856548a003cb44e4c3c4ff48abfb
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-01-30 08:30:02 -0800

    Using View Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def login_helper
+        # have to use .html_safe or this won't process
+       # "<p>My helper</p>".html_safe  
+        if current_user.is_a?(User)
+            link_to "Logout", destroy_user_session_path, method: :delete
+        else
+            link_to "Register", new_user_registration_path
+            link_to "Login", new_user_session_path
+        end  
+    end
 end

6ff97670da29660e824452f825193033bb02cbc4
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-01-30 08:34:41 -0800

    Added br and html safe tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
     def login_helper
-        # have to use .html_safe or this won't process
+        # have to use .html_safe or this won't process. 
        # "<p>My helper</p>".html_safe  
         if current_user.is_a?(User)
             link_to "Logout", destroy_user_session_path, method: :delete
         else
-            link_to "Register", new_user_registration_path
-            link_to "Login", new_user_session_path
+            # need to concat because ruby will return the last item only
+            (link_to "Register", new_user_registration_path) + "<br>".html_safe + 
+            link_to "Login", new_user_session_path)
         end  
     end
 end

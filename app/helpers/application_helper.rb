483d677471f5419fd78c113c4ff92f1b16d36b96
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-02-01 07:54:06 -0800

    Content tag helper to generate HTML Code SavePoint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
         else
             # need to concat because ruby will return the last item only
             (link_to "Register", new_user_registration_path) + "<br>".html_safe + 
-            link_to "Login", new_user_session_path)
+            (link_to "Login", new_user_session_path)
         end  
     end
 end

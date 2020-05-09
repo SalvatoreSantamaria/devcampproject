daabb110972ac28d073c60491fe1b8bce25ae949
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-05-09 07:09:29 -0700

    Building the Comment Form and Required Controller Actions - Part 1 of 2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+    def create
+        @comment = current_user.comments.build(comment_params) #grab current user and build the comment for us, auto associated the user with comment
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content) #whitelist :content comming through
+    end
 end

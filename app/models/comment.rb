5e7540a4f2672c1df22b17403e73c03be881c1ae
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-05-12 06:36:04 -0700

    How to Create a Broadcast with ActionCable in Rails 5

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
   validates :content, presence: true, length: {minimum: 5, maximum: 1000}
+
+  #callback function. calling CommentBroadcastJob
+  after_create_commit { CommentBroadcastJob.perform_later(self)}
 end

bf77a827c6c95e36a1db35fec032f1004ef2e6f5
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-05-21 06:41:07 -0700

    Updating the Source/Session Tracking System for the New Layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,10 +14,10 @@ module ApplicationHelper
         content_tag(:div, "My content", class: "my-class")
     end
 
-    def source_helper(layout_name)
+    def source_helper(styles)
         if session[:source]
-            greeting = "Thanks for visting me from  #{session[:source]} and you are on the #{layout_name} layout!"
-            content_tag(:p, greeting, class: "source-greeting")
+            greeting = "Thanks for visting me from  #{session[:source]}"
+            content_tag(:div, greeting.html_safe, class: styles)
         end 
     end
 

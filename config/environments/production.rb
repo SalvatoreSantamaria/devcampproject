699638089bd178e6c5223e2ed490134a952506bd
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-05-25 07:33:09 -0700

    Configured Action Cable and Redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://salvatore-devcamp-portfolio.herokuapp.com', 'http://salvatore-devcamp-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://salvatore-devcamp-portfolio.herokuapp.com/cable"
 end
